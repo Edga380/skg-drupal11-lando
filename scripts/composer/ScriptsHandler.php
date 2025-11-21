bfd9caa80f2832cfa2b251e6b447eea8c1a0b13a
Edgaras Aleliunas <edgaras.aleliunas@manifesto.co.uk>
2025-11-21 11:04:45 +0000

    Initial src/themes structure, root directory clean up, ScriptsHandler fixes

--- scripts/composer/ScriptsHandler.php
+++ scripts/composer/ScriptsHandler.php
@@ -1,5 +1,10 @@
 <?php
 
+define('C_RED', "\033[31m");
+define('C_GREEN', "\033[32m");
+define('C_YELLOW', "\033[33m");
+define('C_RESET', "\033[0m");
+
 namespace DrupalProject\Composer;
 
 class ScriptsHandler {
@@ -7,11 +12,11 @@ class ScriptsHandler {
      * Copy configured settings.php file from directory src/settings to web/sites/default
      */
     public static function copySettingsFile() {
-        $source = __DIR__ . '/../../src/settings/settings.php'
-        $destination = __DIR__ . '/../../web/sites/default/settings.php'
+        $source = __DIR__ . '/../../src/settings/settings.php';
+        $destination = __DIR__ . '/../../web/sites/default/settings.php';
 
         if(!file_exists($source)) {
-            fwrite(STDERR, "Source settings.php does not exist: $source\n");
+            fwrite(STDERR,  C_RED . "Source settings.php does not exist: $source" . C_RESET . "\n");
         }
 
         $destination = dirname($destination);
@@ -21,9 +26,9 @@ class ScriptsHandler {
         }
 
         if(copy($source, $destination)) {
-            echo "(settings.php) copied successfully from $source to $destination\n";
+            echo C_GREEN ."Copied (settings.php) successfully from $source to $destination" . C_RESET . "\n";
         } else {
-            fwrite(SDTERR, "Failed to copy settings.php from $source to $destination\n");
+            fwrite(STDERR, C_RED . "Failed to copy (settings.php) from $source to $destination" . C_RESET . "\n");
         }
     }
 }

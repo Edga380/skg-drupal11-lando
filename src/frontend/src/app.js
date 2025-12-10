cd9411026e53d19ecd03a5745394e0f68bb8e04e
Edgaras Aleliunas <edgaras.aleliunas@manifesto.co.uk>
2025-12-10 17:22:15 +0000

    feat[SKG-02]: Contact message feedback form added styling

--- src/frontend/src/app.js
+++ src/frontend/src/app.js
@@ -1,11 +1,13 @@
 import "./base.scss";
 
+import contactMessageFeedbackForm from "./components/contact-message-feedback-form";
 import footer from "./components/footer";
 import navigationBar from "./components/navigation-bar";
 import headerSlideshow from "./components/header-slideshow";
 import gamePage from "./components/game-page";
 
 const runComponents = () => {
+  contactMessageFeedbackForm();
   footer();
   navigationBar();
   headerSlideshow();
---
+++ src/frontend/src/components/contact-message-feedback-form/index.js
@@ -0,0 +1,7 @@
+import './contact-message-feedback-form.scss';
+
+const contactMessageFeedbackForm = () => {
+
+}
+
+export default contactMessageFeedbackForm;
